5a1e13062c3c2c56293775b87e2c233d0742e036
김밍슈 <104071568+min9-530@users.noreply.github.com>
2023-09-05 23:44:49 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    if(a % 2 === 1 && b % 2 === 1) {
+        return ((a**2)+(b**2));
+    } else if(a % 2 != 1 && b % 2 != 1) {
+            return Math.abs(a-b);
+        } else
+            return 2*(a+b);
+}
