7df14ac58a91b6ee40578b73b9dfdad1f75992ac
김밍슈 <104071568+min9-530@users.noreply.github.com>
2023-11-09 23:07:13 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,12 @@
+function solution(price) {
+    if(100000 <= price && price < 300000){
+        return Math.floor(price-(price * 0.05));
+    }
+    else if(300000 <= price && price < 500000){
+        return Math.floor(price-(price * 0.1))
+    }
+    else if(500000 <= price)
+        return Math.floor(price-(price * 0.2));
+    else 
+        return price;
+}
